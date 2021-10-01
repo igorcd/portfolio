5bc88f5fffc68b735a44ce7a77b2c67ec0c1c625
Igor Chagas <igor.chagas@vsoft.com.br>
2021-10-01 10:28:34 -0300

    Atualizado imagem de perfil

--- src/assets/img/index.ts
+++ src/assets/img/index.ts
@@ -2,7 +2,7 @@ import avatar from './avatar.jpg';
 import welcomeBg from './welcome-bg.jpg';
 import menus from './menus.jpg';
 import centralApps from './central-apps.png';
-import profile from './profile.png';
+import profile from './profile.svg';
 
 export {
     avatar,
